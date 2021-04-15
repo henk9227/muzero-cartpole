2614a0bf6303b503c959e2528c8c1c6df817e2b1
henk9227 <76836251+henk9227@users.noreply.github.com>
2021-04-15 14:32:13 +0900

    Update muzero.py

--- src/muzero.py
+++ src/muzero.py
@@ -1,20 +1,11 @@
-from src.config import MuZeroConfig, make_cartpole_config
-from src.networks.shared_storage import SharedStorage
-from src.self_play.self_play import run_selfplay, run_eval
-from src.training.replay_buffer import ReplayBuffer
-from src.training.training import train_network
+from config import MuZeroConfig, make_cartpole_config
+from networks.shared_storage import SharedStorage
+from self_play.self_play import run_selfplay, run_eval
+from training.replay_buffer import ReplayBuffer
+from training.training import train_network
 
 
 def muzero(config: MuZeroConfig):
-    """
-    MuZero training is split into two independent parts: Network training and
-    self-play data generation.
-    These two parts only communicate by transferring the latest networks checkpoint
-    from the training to the self-play, and the finished games from the self-play
-    to the training.
-    In contrast to the original MuZero algorithm this version doesn't works with
-    multiple threads, therefore the training and self-play is done alternately.
-    """
     storage = SharedStorage(config.new_network(), config.uniform_network(), config.new_optimizer())
     replay_buffer = ReplayBuffer(config)
 
@@ -28,9 +19,26 @@ def muzero(config: MuZeroConfig):
         print(f"MuZero played {config.nb_episodes * (loop + 1)} "
               f"episodes and trained for {config.nb_epochs * (loop + 1)} epochs.\n")
 
+    storage.save_network_dir(config.nb_training_loop)
+
     return storage.latest_network()
 
 
-if __name__ == '__main__':
-    config = make_cartpole_config()
-    muzero(config)
+def muzero_load_train(config: MuZeroConfig, step: int):
+    storage = SharedStorage(config.new_network(), config.uniform_network(), config.new_optimizer())
+    replay_buffer = ReplayBuffer(config)
+    storage.load_network_dir(step)
+    
+    for loop in range(config.nb_training_loop):
+        print("Training loop", loop)
+        score_train = run_selfplay(config, storage, replay_buffer, config.nb_episodes)
+        train_network(config, storage, replay_buffer, config.nb_epochs)
+
+        print("Train score:", score_train)
+        print("Eval score:", run_eval(config, storage, 50))
+        print(f"MuZero played {config.nb_episodes * (loop + 1)} "
+              f"episodes and trained for {config.nb_epochs * (loop + 1)} epochs.\n")
+
+    # storage.save_network_dir(config.nb_training_loop + step)
+
+    return storage.latest_network()
